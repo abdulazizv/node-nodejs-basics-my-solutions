6456a1305246db7c11f36a3e1888d1880d7dad9b
abdulazizv <abdulazizheir@gmail.com>
2023-04-16 15:55:46 +0500

    fs/list.js added

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import {readdir} from 'fs/promises'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
 const list = async () => {
-    // Write your code here 
+    try {
+        let array = [];
+        let folder = __dirname + '/files'
+        const files = await readdir(folder);
+        Promise.all(
+          files.map((i) => {
+            array.push(i)
+          })
+        );
+        console.log(array)
+    } catch (error) {
+        throw new Error("FS operation failed")
+    } 
 };
 
 await list();
