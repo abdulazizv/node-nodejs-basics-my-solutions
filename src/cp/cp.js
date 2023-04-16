ae9bd2df711bd696e8a7a3b8c2c7aad8e7f279d4
abdulazizv <abdulazizheir@gmail.com>
2023-04-16 16:03:24 +0500

    addd

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,26 @@
+import {spawn} from 'child_process'
+import { fileURLToPath } from 'url';
+import {dirname} from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    let isInity = false
+    
+    const childProcess = spawn(`node`,[`${dirname}/files/script.js`,...args.split(' ')])
+    process.stdin.on('data',(msg) => {
+        childProcess.stdin.write(msg)
+    })
+    childProcess.stdout.on('data',(data) => {
+        console.log(data.toString())
+        if(!isInity) {
+            console.log('Write something to console...')
+            console.log('\n')
+            isInity = true
+
+        }
+    })
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess('--silent --all');
