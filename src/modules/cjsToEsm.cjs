9f0c3ec6dbd613f1b30306cd67337da22ebe4a1a
abdulazizv <abdulazizheir@gmail.com>
2023-04-16 16:06:11 +0500

    cjsToEsm.js

--- src/modules/cjsToEsm.cjs
+++ src/modules/cjsToEsm.cjs
@@ -1,40 +1,31 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
 
-const random = Math.random();
+import path from 'path'
+import {dirname} from 'path'
+import {release,version} from 'os'
+import { createServer as createServerHttp } from 'http';
 
-let unknownObject;
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-if (random > 0.5) {
-    unknownObject = require('./files/a.json');
-} else {
-    unknownObject = require('./files/b.json');
-}
+import './files/c.js';
 
-console.log(`Release ${release()}`);
-console.log(`Version ${version()}`);
-console.log(`Path segment separator is "${path.sep}"`);
+const random = Math.random()
 
-console.log(`Path to current file is ${__filename}`);
-console.log(`Path to current directory is ${__dirname}`);
+const {default: unknownObject} = random > 0.5 ?
+    await import('./files/a.json',{assert:{type:'json'}}):
+    await import('./files/b.json',{assert:{type:'json'}});
 
-const myServer = createServerHttp((_, res) => {
-    res.end('Request accepted');
-});
+console.log(`Release ${release()}`)
+console.log(`Version ${version()}`)
+console.log(`Path segment separator is "${path.sep}"`)
 
-const PORT = 3000;
+console.log(`Path to current file is ${__filename}`)
+console.log(`Path to current directory is ${__dirname}`)
 
-console.log(unknownObject);
-
-myServer.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
-});
-
-module.exports = {
+const createServer = createServerHttp((_,res) => {
+    res.end(`Request accepted`)
+})
+export {
     unknownObject,
-    myServer,
-};
-
+    createServer
+}
