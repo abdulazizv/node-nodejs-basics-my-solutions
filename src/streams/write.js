95f9c0e18d3a0bd480073007f33bbcad93b50b94
abdulazizv <abdulazizheir@gmail.com>
2023-04-16 16:07:34 +0500

    commited streams/read.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,9 @@
+import {createReadStream} from 'fs'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const write = async () => {
     // Write your code here 
 };
